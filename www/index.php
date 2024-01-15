df61d536a0f822461938985665c1b2cca6a810b9
Willem Bijker <willem@bijker.co.za>
2024-01-15 07:41:30 +0200

    test tokenizer

--- www/index.php
+++ www/index.php
@@ -11,7 +11,14 @@ require_once 'HtmlTemplateNode.php';
 require_once 'ForTemplateNode.php';
 require_once 'IfTemplateNode.php';
 require_once 'TextTemplateNode.php';
+require_once 'HtmlTokenizer.php';
 
 session_start();
 
-renderPage(new HomePage());
+//renderPage(new HomePage());
+
+ob_start();
+require "views/home.php";
+$html = ob_get_clean();
+
+print_r(HtmlTokenizer::tokenizeHtml('<div    class  =     "bg-red-500" value=\'34\'><img src="image1.jpg"/>  <span> very good sir </span>  </div>'));
