77cdbae5e383e3170e32d2182c98ca3ce862d5d2
Willem Bijker <willem@itecho.co.za>
2024-09-06 18:14:24 +0200

    working Levenshtein

--- spwa/src/Dom/Levenshtein.php
+++ spwa/src/Dom/Levenshtein.php
@@ -5,57 +5,96 @@ namespace Spwa\Dom;
 
 class Levenshtein
 {
+    const DELETE = 0;
+    const INSERT = 1;
+    const SKIP = 2;
+    const SUBSTITUTE = 3;
+
     // modified version of the levenshtein algorithm
     static function populate(array $arr1, array $arr2, callable $key): array
     {
         $n = count($arr1);
         $m = count($arr2);
         $dp = array_fill(0, $n + 1, array_fill(0, $m + 1, 0));
+
         for ($i = 0; $i <= $n; $i++) {
             for ($j = 0; $j <= $m; $j++) {
                 if ($i == 0) {
-                    $dp[$i][$j] = $j;
+                    $dp[$i][$j] = [$j, self::INSERT];
                     continue;
                 }
                 if ($j == 0) {
-                    $dp[$i][$j] = $i;
+                    $dp[$i][$j] = [$i, self::DELETE];
                     continue;
                 }
-                // if they are the same, skip, if not substitute
-                $cost = $key($arr1[$i - 1]) == $key($arr2[$j - 1]) ? 0 : 1;
-                $dp[$i][$j] = min(
-                    $dp[$i - 1][$j] + 1,            // Deletion
-                    $dp[$i][$j - 1] + 1,            // Insertion
-                    $dp[$i - 1][$j - 1] + $cost    // Skip or Substitution
-                );
+                $same = $key($arr1[$i - 1]) == $key($arr2[$j - 1]);
+
+                $neighbours = [
+                    [$dp[$i - 1][$j - 1][0] + ($same ? 0 : 1), ($same ? self::SKIP : self::SUBSTITUTE)],
+                    [$dp[$i - 1][$j][0], self::DELETE],
+                    [$dp[$i][$j - 1][0], self::INSERT]
+                ];
+
+                // take the minimum of the neighbours
+                $min = array_reduce($neighbours, fn($acc, $item) => $item[0] < $acc[0] ? $item : $acc, $neighbours[0]);
+                $dp[$i][$j] = [$min[0] + ($same ? 0 : 1), $min[1]];
             }
         }
         return $dp;
     }
 
-    static function debug(array $arr1, array $arr2, callable $key)
+    static function debug(array $from, array $to, callable $key)
     {
-        $dp = self::populate($arr1, $arr2, $key);
+        $dp = self::populate($from, $to, $key);
         // render a table with the dp array
         echo "<table border='1'>";
         echo "<tr><td></td><td>-</td>";
-        foreach ($arr2 as $item) {
-            echo "<td>$item</td>";
+        foreach ($to as $item) {
+            echo "<td>$item[0]</td>";
         }
         echo "</tr>";
-        for ($i = 0; $i <= count($arr1); $i++) {
+        for ($i = 0; $i <= count($from); $i++) {
             echo "<tr>";
             if ($i == 0) {
                 echo "<td>-</td>";
             } else {
-                echo "<td>{$arr1[$i - 1]}</td>";
+                echo "<td>{$from[$i - 1][0]}</td>";
             }
-            for ($j = 0; $j <= count($arr2); $j++) {
-                echo "<td>{$dp[$i][$j]}</td>";
+            for ($j = 0; $j <= count($to); $j++) {
+                echo "<td>{$dp[$i][$j][0]} , {$dp[$i][$j][1]}</td>";
             }
             echo "</tr>";
         }
         echo "</table>";
+
+        $i = count($from);
+        $j = count($to);
+
+        while ($i > 0 || $j > 0) {
+            $node = $dp[$i][$j];
+            print_r($node);
+
+            switch ($node[1]) {
+                case self::INSERT:
+                    echo "Insert {$to[$j - 1]}<br>";
+                    $j--;
+                    break;
+                case self::DELETE:
+                    echo "delete {$from[$i-1]}<br>";
+                    $i--;
+                    break;
+                case self::SUBSTITUTE:
+                    echo "Sub {$from[$i-1]} with {$to[$j-1]}<br>";
+                    $i--;
+                    $j--;
+                    break;
+                case self::SKIP:
+                    echo "SKIP {$to[$j - 1]}<br>";
+                    $i--;
+                    $j--;
+                    break;
+            }
+        }
     }
 
 
