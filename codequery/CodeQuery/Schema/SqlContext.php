b61ecb848a34e67dc5c5d79d1b910be998ba977c
Willem Bijker <willem@itecho.co.za>
2025-08-09 11:00:59 +0200

    SqlContext build table instance

--- codequery/CodeQuery/Schema/SqlContext.php
+++ codequery/CodeQuery/Schema/SqlContext.php
@@ -1,8 +1,27 @@
 <?php
+
 namespace CodeQuery\Schema;
 
 
 class SqlContext
 {
+    private array $cache = [];
+
+    public function tableInstance(string $tableClass): Table
+    {
+        $hit = $this->cache[$tableClass] ?? null;
+        if ($hit) {
+            return $hit;
+        }
+
+        $table = new $tableClass($this);
+        if (!$table instanceof Table) {
+            throw new \InvalidArgumentException("Class $tableClass must be an instance of " . Table::class);
+        }
+        // invoke the builder to build the table structure
+        $table->buildTable(new TableBuilder($table));
+        $this->cache[$tableClass] = $table;
+        return $table;
+    }
 
 }
